132ed15ebed393a2378c14096dbaa19d7c9b342c
Ritesh Swain <152496676+Ritesh001-Git@users.noreply.github.com>
2026-02-23 00:08:43 +0530

    Added new fields for docker id

--- backend/models.py
+++ backend/models.py
@@ -1,11 +1,16 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy.sql import func
 from database import Base
 
 class Project(Base):
     __tablename__ = "projects"
 
-    id = Column(Integer, primary_key=True)
-    repo_url = Column(String)
-    container_name = Column(String)
-    port = Column(Integer)
-    status = Column(String)
+    id = Column(Integer, primary_key=True, index=True)
+    repo_url = Column(String, nullable=False) #
+    container_name = Column(String, unique=True, nullable=False) #
+    port = Column(Integer, unique=True, nullable=False) #
+    status = Column(String, default="PENDING") #
+    
+    # New Industry-Level Fields
+    docker_id = Column(String, nullable=True)  # Store the actual Docker Container ID
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
