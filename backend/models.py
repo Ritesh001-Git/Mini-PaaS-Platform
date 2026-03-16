fb8d3e02a38f4199f46e1180167cb95175f6abeb
Ritesh Swain <152496676+Ritesh001-Git@users.noreply.github.com>
2026-03-16 23:17:31 +0530

    User Model Updated

--- backend/models.py
+++ backend/models.py
@@ -13,6 +13,7 @@ class User(Base):
     id = Column(Integer, primary_key=True, index=True)
     email = Column(String, unique=True, nullable=False, index=True)
     password = Column(String, nullable=False)
+    # provider = Column(String, default="local")
 
     created_at = Column(DateTime(timezone=True), server_default=func.now())
 
